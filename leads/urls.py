c3e80c50dd4319a5ac99e39370227be4cd235ed0
irohanrajput <itusharsingh27@gmail.com>
2023-12-09 23:33:57 +0530

    changes

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_page
+from .views import lead_list
 
-app_name= "leads"
+app_name = 'leads'
 
 urlpatterns = [
-  path('', "templates/second_page.html")
+  path('', lead_list, name="second_page")
 ]
+
